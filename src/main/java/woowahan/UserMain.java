406d60305c5045645f3c209b5cfba8b7b3c8ba8b
SUNGBUMPARK <psbum1609@gmail.com>
2019-06-06 18:31:10 +0900

    java bean, 회원 목록 기능 구현

--- src/main/java/woowahan/UserMain.java
+++ src/main/java/woowahan/UserMain.java
@@ -3,23 +3,26 @@ package woowahan;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
 
-public class HelloWorld {
+public class UserMain {
     public static void main(String[] args) {
         staticFiles.location("/static");
 
-        get("/hello", (req, res) -> {
-            return "Get Hello : " + req.queryParams("name") + " 나이 : " + req.queryParams("age");
-        });
+        List<User> users = new ArrayList<>();
+        post("/users", (req, res) -> {
+            User user = new User();
+            user.setName(req.queryParams("name"));
+            user.setAge(req.queryParams("age"));
+            users.add(user);
 
-        post("/hello", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
-            model.put("name", req.queryParams("name"));
-            model.put("age", req.queryParams("age"));
+            model.put("users", users);
 
             return render(model, "result.html");
         });
